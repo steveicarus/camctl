d48df6dd2f5cc63cbc72f3eb188686df3376cc55
Stephen Williams <steve@icarus.com>
2009-03-01 16:08:46 -0800

    More elaborate bundling for the Mac.

--- scripts/mac_package.sh
+++ scripts/mac_package.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
+version='20090224'
+
 # This script makes an installable package of the camtool. Run this script
 # from the root of the source (the directory that contains the camtool
 # directory and the scripts directory) and it will make the package.
 
+# Make a copy of the Icarus Camera Control application bundle, and run the
+# macdeployqt program to cause all the necessary QT files to be bundled in.
 cp -r camtool/camtool.app .
-
 scripts/macdeployqt/macdeployqt camtool.app
 
+# The basename is the name of the install application bundle.
 basename='Icarus Camera Control'
-mv camtool.app "$basename.app"
 
-hdiutil create "$basename".dmg -srcfolder "$basename".app -format UDZO -volname "$basename"
+# The packname is the file name to use for packages and images. This
+# will have spaces removed.
+packname=IcarusCameraControl-"$version"
 
-rm -rf "$basename".app
+mkdir scratch
+
+mv camtool.app scratch/"$basename.app"
+
+cp scripts/mac_README.rtf scratch/README.rtf
+
+mkdir scratch/'Source Code'
+cp camctl-"$version".tar.gz scratch/'Source Code'
+
+hdiutil create "$packname".dmg -srcfolder scratch -format UDZO -volname "$packname"
+
+rm -rf scratch
